94f2c014311f51bd9275f846ac5f758d04d72c02
Jiahao <8275280+jiahao-c@users.noreply.github.com>
2022-06-28 13:18:59 -0400

    init

--- next.config.js
+++ next.config.js
@@ -1,6 +1,24 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const path = require('path');
+const withLess = require('next-with-less');
+const withTM = require('next-transpile-modules')([
+  '@arco-design/web-react',
+]);
 
-module.exports = nextConfig
+
+module.exports = withLess(
+  withTM({
+    webpack: (config) => {
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      });
+
+      config.resolve.alias['@'] = path.resolve(__dirname, './src');
+
+      return config;
+    },
+    pageExtensions: ['tsx'],
+  })
+);
